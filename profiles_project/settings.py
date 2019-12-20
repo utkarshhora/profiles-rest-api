c0f88dd811dbb0c1c03be1b0b2c28a8a081c8fbf
Utkarsh Hora <utkarshhora.99@gmail.com>
2019-12-21 03:11:15 +0530

    Ready for config again

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'm&3b@fxrnd152^go5@wtbo3ast!19tbvx9b0+tbtzl_q6z(l9@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
-ALLOWED_HOSTS = ['ec2-3-15-205-69.us-east-2.compute.amazonaws.com',
-                 '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -122,5 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
 STATIC_ROOT = 'static/'
